01138cfdc3ec5405d3deaa1ec7424027082ae327
Louis Sobel <sobel@mit.edu>
2014-03-24 15:50:08 -0400

    ok

--- makejank.py
+++ makejank.py
@@ -7,8 +7,10 @@ import jinja2
 from loaders import *
 from renderer import Renderer
 from loader_manager import LoaderManager
+from caches import FilesystemCache
 
-lm = LoaderManager()
+cache = FilesystemCache('.makejank_cache')
+lm = LoaderManager(cache)
 lm.register(YamlLoader())
 lm.register(CSSLoader())
 lm.register(JSLoader())

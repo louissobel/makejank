3298d52010e11b39900c405bf26dd3234fc929c6
Louis Sobel <sobel@mit.edu>
2014-04-07 19:29:56 -0400

    makejank loader

--- makejank/loaders/makejank_loader.py
+++ makejank/loaders/makejank_loader.py
@@ -4,14 +4,9 @@ from makejank.dependency_detector import DependencyDetector
 
 from . import BaseLoader
 
-PRODUCT_NAME_TEMPLATE = "%s:makejank"
-
 class MakejankLoader(BaseLoader):
 
     tag = 'makejank'
-
-    def product(self, env, filename, kwargs):
-        return PRODUCT_NAME_TEMPLATE % filename
 
     def dependencies(self, env, filename, kwargs):
         deps = DependencyDetector(env).get_source_and_process(filename)

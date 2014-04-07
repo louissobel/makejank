94bfcaa68b77132e242ba7f9df3f8bfd08a175ec
Louis Sobel <sobel@mit.edu>
2014-04-07 19:29:23 -0400

    test makejank tests makejank loader

--- test/test_loaders/test_makejank_loader.py
+++ test/test_loaders/test_makejank_loader.py
@@ -5,24 +5,27 @@ import unittest
 import tempfile
 import shutil
 
-from makejank import Makejank
+from makejank.loaders import *
+from makejank.environment import Environment
 
 import test.helpers
 
-def setup_module():
-    global mj
-    global tempdir
-
-    tempdir = tempfile.mkdtemp()
-    mj = Makejank(
-        cache_dir=tempdir,
-        base_dir=test.helpers.datadir(),
-    )
+env = Environment(
+    rootdir=test.helpers.datadir(),
+    loaders = [
+        YamlLoader(),
+        CSSLoader(),
+        JSLoader(),
+        ImgLoader(),
+        MakejankLoader(),
+    ]
+)
 
 class TestDeps(unittest.TestCase):
     def runTest(self):
         # Heavily dependent on contents of page.
-        deps = mj.get_deps('makejank', 'example/page.html')
+        loader = MakejankLoader()
+        deps = loader.dependencies(env, 'example/page.html', {})
         self.assertEquals(deps, set(map(test.helpers.test_datafile, [
             'example/smiley.jpg',
             'example/base.html',
@@ -39,8 +42,5 @@ class TestRender(unittest.TestCase):
     def runTest(self):
         # For now, just test that it doesn't throw an error,
         # :/ jank.
-        rendered = mj.render('makejank', 'example/page.html')
-
-def teardown_module():
-    global tempdir
-    shutil.rmtree(tempdir)
+        loader = MakejankLoader()
+        rendered = loader.load(env, 'example/page.html', {})

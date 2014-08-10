86caff8766f0d540e58bf2fee1c3dd5e65503860
Louis Sobel <sobel@mit.edu>
2014-08-09 19:45:01 -0700

    set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name='makejank',
+    version='0.0.1',
+    packages=find_packages(exclude=['test']),
+    scripts=['bin/makejank'],
+)
